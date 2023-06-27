5c822c29547f57d93751847b347d94365da64e4f
Faisal Memon <faisalmemonuk@yahoo.com>
2023-06-27 19:51:13 +0100

    Initial Commit

---
+++ trivial-red-cube-visionos-app/trivial_red_cube_visionos_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  trivial_red_cube_visionos_appApp.swift
+//  trivial-red-cube-visionos-app
+//
+//  Created by Faisal Memon on 27/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct trivial_red_cube_visionos_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
